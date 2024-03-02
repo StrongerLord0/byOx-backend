5ed7b0985ee97f541dabc7b7ecc7f9e71d63fd58
eliangerard <eliangerardgg@gmail.com>
2024-03-02 17:17:33 -0600

    first commit

---
+++ start.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from deepface import DeepFace
+import cv2
+import numpy as np
+import io
+
+app = Flask(__name__)
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+	file = request.files['file']
+	in_memory_file = io.BytesIO()
+	file.save(in_memory_file)
+	data = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
+	color_image_flag = 1
+	img = cv2.imdecode(data, color_image_flag)
+
+	result = DeepFace.analyze(img, actions = ['emotion'])
+
+	return jsonify(result), 200
+
+if __name__ == '__main__':
+	app.run(debug=True)
